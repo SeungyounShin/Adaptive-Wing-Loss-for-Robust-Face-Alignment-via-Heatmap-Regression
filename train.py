136e8749b546e3a0fa72916c9f828cc01162164e
Seungyoun, Shin <ssy10011218@gmail.com>
2020-06-07 12:47:47 +0900

    minor change
    image axes swap changed

--- train.py
+++ train.py
@@ -11,14 +11,27 @@ import numpy as np
 num_epochs = 240
 vis_result = True
 batch_size = 8
+pretrained = True
+pretrained_start_epoch = 110
+start_lr = 1e-6
+pretrain_path = "./ckpt/100.pth"
 if(vis_result):
     import matplotlib.pyplot as plt
+W = 5
+omega = 300
+epsilon= 2
+
+num_epochs -= pretrained_start_epoch
 
 #model load
-model  = hg(num_stacks=4,
-            num_blocks=2,
-            num_classes=68+1)
+if pretrained:
+    model = torch.load(pretrain_path)
+else:
+    model  = hg(num_stacks=4,
+                num_blocks=2,
+                num_classes=68+1)
 model = model.cuda()
+print("model loaded")
 
 #dataset load
 dataset_path = "./data/300W"
@@ -26,13 +39,14 @@ train_dataset = face300W(dataset_path)
 dataiter = len(train_dataset)//batch_size
 train_loader = torch.utils.data.DataLoader(train_dataset, batch_size=batch_size,
                                            shuffle=True, num_workers=0)
+print("images : ",len(train_dataset))
 
 #loss define
 criterion = Loss_weighted()
 
 #optim setting
-optimizer = optim.RMSprop(model.parameters(), lr=1e-4, weight_decay=1e-5, momentum=0)
-scheduler = optim.lr_scheduler.MultiStepLR(optimizer,  milestones=[80,160], gamma=0.1)
+optimizer = optim.RMSprop(model.parameters(), lr=start_lr, weight_decay=1e-5, momentum=0)
+scheduler = optim.lr_scheduler.MultiStepLR(optimizer,  milestones=[60,120], gamma=0.1)
 
 #train start!
 print("train start!")
@@ -41,7 +55,7 @@ for epoch in range(num_epochs):
     losses = list()
     for iteration,sample in enumerate(train_loader):
         img,hmap,M,pts = sample
-        img = img.transpose(1,-1)
+        img = img.permute(0,3,1,2)
         img,hmap,M = img.cuda(), hmap.cuda(), M.cuda()
 
         out = model(img)
@@ -58,8 +72,8 @@ for epoch in range(num_epochs):
 
         if vis_result and (iteration %20 == 0):
             y_hat = out[-1][0,:68].detach().cpu().numpy()
-            plt.imshow(cv2.resize(img.transpose(-1,1)[0].cpu().numpy(), dsize=(64, 64), interpolation=cv2.INTER_LINEAR))
-            plt.imshow(np.max(y_hat, axis=0),alpha=0.5)
+            plt.imshow(img.permute(0,2,3,1)[0].cpu().numpy())
+            plt.imshow(cv2.resize(np.max(y_hat, axis=0), dsize=(256, 256), interpolation=cv2.INTER_LINEAR),alpha=0.5)
             plt.show(block=False)
             plt.pause(3)
             plt.close()
